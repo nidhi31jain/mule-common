19b39de4e0db120d3f699349270e6d3183508e57
Ivan Postolski <ivan.postolski@mulesource.com>
2013-06-18 20:17:04 -0300

    Modified the way metadata filter dynamic attributes for less devkit code generation. part two.

---
+++ src/main/java/org/mule/common/metadata/MetaDataQueryFilterVisitor.java
@@ -0,0 +1,61 @@
+package org.mule.common.metadata;
+
+import org.mule.common.metadata.datatype.DataType;
+import org.mule.common.query.Field;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Visitor used to filter metadata fields
+ */
+public class MetaDataQueryFilterVisitor implements MetaDataModelVisitor {
+    private List<Field> fields;
+    private MetaDataModel resultModel;
+
+    public MetaDataQueryFilterVisitor(List<Field> fields) {
+        this.fields = fields;
+        this.resultModel = new DefaultSimpleMetaDataModel(DataType.VOID);
+    }
+
+
+    @Override
+    public void visitPojoModel(PojoMetaDataModel pojoMetaDataModel) {
+        //DO NOTHING
+    }
+
+    @Override
+    public void visitListMetaDataModel(ListMetaDataModel listMetaDataModel) {
+        MetaDataQueryFilterVisitor child = new MetaDataQueryFilterVisitor(fields);
+        listMetaDataModel.getElementModel().accept(child);
+        resultModel = new DefaultListMetaDataModel(child.filteringResult().getPayload());
+    }
+
+    @Override
+    public void visitSimpleMetaDataModel(SimpleMetaDataModel simpleMetaDataModel) {
+        //DO NOTHING
+    }
+
+    @Override
+    public void visitStaticMapModel(ParameterizedMapMetaDataModel parameterizedMapMetaDataModel) {
+        //DO NOTHING
+    }
+
+    @Override
+    public void visitDynamicMapModel(DefinedMapMetaDataModel definedMapMetaDataModel) {
+        if (fields == null) return;
+        Map<String,MetaDataModel> newMapModel = new HashMap<String, MetaDataModel>();
+        for(Field f: fields) {
+            MetaDataModel fieldModel = definedMapMetaDataModel.getValueMetaDataModel(f.getName());
+            if (fieldModel != null) {
+                newMapModel.put(f.getName(),fieldModel);
+            }
+        }
+        resultModel = new DefaultQueryResultMetaDataModel(new DefaultDefinedMapMetaDataModel(newMapModel,definedMapMetaDataModel.getName()));
+    }
+
+    public MetaData filteringResult() {
+        return new DefaultMetaData(resultModel);
+    }
+}
