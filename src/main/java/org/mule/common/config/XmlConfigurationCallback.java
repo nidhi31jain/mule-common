3ae6f29c434ca3328d8a6873c008b9a3585a0d1f
Mariano de Achaval <achaval@gmail.com>
2013-06-27 18:48:19 -0300

    Adding support for property placeholders

--- src/main/java/org/mule/common/config/XmlConfigurationCallback.java
+++ src/main/java/org/mule/common/config/XmlConfigurationCallback.java
@@ -1,11 +1,18 @@
 
 package org.mule.common.config;
 
+import java.util.Map;
+
 import org.w3c.dom.Element;
 
 public interface XmlConfigurationCallback
 {
+
     Element getGlobalElement(String globalElementName);
 
     String getSchemaLocation(String namespaceUri);
+
+    Element[] getPropertyPlaceholders();
+
+    Map<String, String> getEnvironmentProperties();
 }
