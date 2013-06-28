45a680fa1938c3ba92d2680ee3c692766f3f9d5f
Jorge DAlessandro <jorge.dalessandro@mulesource.com>
2013-06-28 14:41:46 -0300

    Adding DefaultOperatorVisitor.

---
+++ src/main/java/org/mule/common/query/DefaultOperatorVisitor.java
@@ -0,0 +1,35 @@
+package org.mule.common.query;
+
+import org.mule.common.query.expression.OperatorVisitor;
+
+public class DefaultOperatorVisitor implements OperatorVisitor {
+	@Override
+	public String lessOperator() {
+		return " < ";
+	}
+
+	@Override
+	public String greaterOperator() {
+		return " > ";
+	}
+
+	@Override
+	public String lessOrEqualsOperator() {
+		return " <= ";
+	}
+
+	@Override
+	public String equalsOperator() {
+		return " = ";
+	}
+
+	@Override
+	public String notEqualsOperator() {
+		return " <> ";
+	}
+
+	@Override
+	public String greaterOrEqualsOperator() {
+		return " >= ";
+	}
+}
