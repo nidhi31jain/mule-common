3f171c01970b1759ce25b4fb66fb1f01d7244e74
Lautaro Fernandez <fernandez.lautaro@gmail.com>
2013-07-26 14:59:34 -0300

    adding tests for DefaultSimpleMetaDataModel

--- src/test/java/org/mule/common/metadata/test/DefaultSimpleMetaDataModelTestCase.java
+++ src/test/java/org/mule/common/metadata/test/DefaultSimpleMetaDataModelTestCase.java
@@ -10,11 +10,9 @@
 
 package org.mule.common.metadata.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.fail;
 
 import org.mule.common.metadata.DefaultSimpleMetaDataModel;
+import org.mule.common.metadata.FieldMetaDataModel;
 import org.mule.common.metadata.SimpleMetaDataModel;
 import org.mule.common.metadata.datatype.DataType;
 
@@ -24,6 +22,11 @@ import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
+import org.mule.common.query.expression.EqualsOperator;
+import org.mule.common.query.expression.LikeOperator;
+import org.mule.common.query.expression.Operator;
+
+import static org.junit.Assert.*;
 
 public class DefaultSimpleMetaDataModelTestCase
 {
@@ -37,6 +40,57 @@ public class DefaultSimpleMetaDataModelTestCase
         List<DataType> complexTypes = new ArrayList<DataType>(Arrays.asList(DataType.values()));
         complexTypes.removeAll(simpleDataTypes);
         complexDataTypes = Collections.unmodifiableList(complexTypes); 
+    }
+
+    @Test
+    public void testFieldCapabilityWithDataType()
+    {
+        FieldMetaDataModel smdm = new DefaultSimpleMetaDataModel(DataType.BOOLEAN);
+        assertTrue(smdm.isSelectCapable());
+        assertTrue(smdm.isWhereCapable());
+        assertTrue(smdm.isSortCapable());
+        assertFalse(smdm.getSupportedOperators().isEmpty());
+
+        smdm = new DefaultSimpleMetaDataModel(DataType.VOID);
+        assertTrue(smdm.isSelectCapable());
+        assertFalse(smdm.isWhereCapable());
+        assertTrue(smdm.isSortCapable());
+        assertTrue(smdm.getSupportedOperators().isEmpty());
+    }
+
+    @Test
+    public void testFieldCapabilityWithDataTypeBoolAndOperators()
+    {
+        FieldMetaDataModel smdm = new DefaultSimpleMetaDataModel(DataType.BOOLEAN, false, false);
+        assertFalse(smdm.isSelectCapable());
+        assertTrue(smdm.isWhereCapable());
+        assertFalse(smdm.isSortCapable());
+        assertFalse(smdm.getSupportedOperators().isEmpty());
+
+        smdm = new DefaultSimpleMetaDataModel(DataType.BOOLEAN, new ArrayList<Operator>());
+        assertTrue(smdm.isSelectCapable());
+        assertFalse(smdm.isWhereCapable());
+        assertTrue(smdm.isSortCapable());
+        assertTrue(smdm.getSupportedOperators().isEmpty());
+
+        smdm = new DefaultSimpleMetaDataModel(DataType.STRING, false, false, new ArrayList<Operator>());
+        assertFalse(smdm.isSelectCapable());
+        assertFalse(smdm.isWhereCapable());
+        assertFalse(smdm.isSortCapable());
+        assertTrue(smdm.getSupportedOperators().isEmpty());
+
+        smdm = new DefaultSimpleMetaDataModel(DataType.STRING, Arrays.<Operator>asList(new EqualsOperator(), new LikeOperator()));
+        assertTrue(smdm.isSelectCapable());
+        assertTrue(smdm.isWhereCapable());
+        assertTrue(smdm.isSortCapable());
+        assertFalse(smdm.getSupportedOperators().isEmpty());
+
+        smdm = new DefaultSimpleMetaDataModel(DataType.STRING, false, false, new ArrayList<Operator>());
+        assertFalse(smdm.isSelectCapable());
+        assertFalse(smdm.isWhereCapable());
+        assertFalse(smdm.isSortCapable());
+        assertTrue(smdm.getSupportedOperators().isEmpty());
+
     }
 
     @Test
