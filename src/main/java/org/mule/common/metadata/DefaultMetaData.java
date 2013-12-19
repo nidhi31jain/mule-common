f10a7bffdf2edd1c6795abce50910407b0404eaf
Mariano de Achaval <mariano.achaval@mulesoft.com>
2013-12-19 16:35:30 -0300

    STUDIO-4208 Adding a new constructor for studio usability peropuse

--- src/main/java/org/mule/common/metadata/DefaultMetaData.java
+++ src/main/java/org/mule/common/metadata/DefaultMetaData.java
@@ -14,42 +14,64 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DefaultMetaData implements MetaData {
+public class DefaultMetaData implements MetaData
+{
 
     private MetaDataModel payload;
     private Map<MetaDataPropertyScope, Map<String, MetaDataModel>> properties;
 
 
-    public DefaultMetaData(MetaDataModel payload) {
+    public DefaultMetaData(MetaDataModel payload)
+    {
         this.payload = payload;
         this.properties = new HashMap<MetaDataPropertyScope, Map<String, MetaDataModel>>();
         initProperties();
     }
 
-    private void initProperties() {
+    public DefaultMetaData(DefaultMetaData oldMetadata, MetaDataModel payload)
+    {
+        this(payload);
+        addAll(oldMetadata, MetaDataPropertyScope.FLOW);
+        addAll(oldMetadata, MetaDataPropertyScope.SESSION);
+        addAll(oldMetadata, MetaDataPropertyScope.INBOUND);
+        addAll(oldMetadata, MetaDataPropertyScope.OUTBOUND);
+    }
+
+    private void addAll(DefaultMetaData oldMetadata, MetaDataPropertyScope propertyScope)
+    {
+        properties.get(propertyScope).putAll(oldMetadata.getProperties(propertyScope));
+    }
+
+    private void initProperties()
+    {
         MetaDataPropertyScope[] values = MetaDataPropertyScope.values();
-        for (MetaDataPropertyScope value : values) {
+        for (MetaDataPropertyScope value : values)
+        {
             properties.put(value, new HashMap<String, MetaDataModel>());
         }
     }
 
     @Override
-    public MetaDataModel getPayload() {
+    public MetaDataModel getPayload()
+    {
         return payload;
     }
 
     @Override
-    public Map<String, MetaDataModel> getProperties(MetaDataPropertyScope scope) {
+    public Map<String, MetaDataModel> getProperties(MetaDataPropertyScope scope)
+    {
         return Collections.unmodifiableMap(properties.get(scope));
     }
 
     @Override
-    public void addProperty(MetaDataPropertyScope scope, String name, MetaDataModel propertyModel) {
+    public void addProperty(MetaDataPropertyScope scope, String name, MetaDataModel propertyModel)
+    {
         properties.get(scope).put(name, propertyModel);
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode()
+    {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((payload == null) ? 0 : payload.hashCode());
@@ -57,19 +79,38 @@ public class DefaultMetaData implements MetaData {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null) return false;
-        if (!(obj instanceof DefaultMetaData)) return false;
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (!(obj instanceof DefaultMetaData))
+        {
+            return false;
+        }
         DefaultMetaData other = (DefaultMetaData) obj;
-        if (payload == null) {
-            if (other.payload != null) return false;
-        } else if (!payload.equals(other.payload)) return false;
+        if (payload == null)
+        {
+            if (other.payload != null)
+            {
+                return false;
+            }
+        }
+        else if (!payload.equals(other.payload))
+        {
+            return false;
+        }
         return true;
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "DefaultMetaData: { payload: " + ((payload != null) ? payload.toString() : "null") + " }";
     }
 }
