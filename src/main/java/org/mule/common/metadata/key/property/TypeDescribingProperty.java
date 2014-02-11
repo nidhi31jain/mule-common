7e948810073b1a9f222c4eb220272c64de02d254
Lautaro Fernandez <fernandez.lautaro@gmail.com>
2014-02-11 19:21:47 -0300

    Fix DEVKIT-537 New property for MetaDataKey (cherry-picked from 9a0d51d)

---
+++ src/main/java/org/mule/common/metadata/key/property/TypeDescribingProperty.java
@@ -0,0 +1,28 @@
+package org.mule.common.metadata.key.property;
+
+/**
+ * Property used between DevKit and the connector. This property will help the connector's developer to discriminate
+ * between input or output metadata
+ */
+public class TypeDescribingProperty implements MetaDataKeyProperty {
+
+    private TypeScope typeScope;
+    private String method;
+
+    public TypeDescribingProperty(TypeScope typeScope, String method) {
+        this.typeScope = typeScope;
+        this.method = method;
+    }
+
+    public TypeScope getTypeScope() {
+        return typeScope;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public enum TypeScope{
+        INPUT, OUTPUT;
+    }
+}
