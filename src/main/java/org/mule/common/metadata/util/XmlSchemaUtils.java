48f1fa3ded6f1c15030204522097a5d11973f346
Mariano Achaval <mariano.achaval@mulesoft.com>
2014-06-19 10:26:37 -0300

    Fixing SE-944

---
+++ src/main/java/org/mule/common/metadata/util/XmlSchemaUtils.java
@@ -0,0 +1,36 @@
+package org.mule.common.metadata.util;
+
+import org.mule.common.metadata.XmlMetaDataFieldFactory;
+
+import java.util.List;
+
+import org.apache.xmlbeans.SchemaTypeLoader;
+import org.apache.xmlbeans.SchemaTypeSystem;
+import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.XmlObject;
+import org.apache.xmlbeans.XmlOptions;
+import org.apache.xmlbeans.impl.schema.BuiltinSchemaTypeSystem;
+import org.apache.xmlbeans.impl.schema.SchemaTypeLoaderImpl;
+import org.apache.xmlbeans.impl.schema.SchemaTypeSystemCompiler;
+
+
+public class XmlSchemaUtils
+{
+
+    public static SchemaTypeSystem getSchemaTypeSystem(List<String> schemas) throws XmlException
+    {
+        final XmlOptions options = new XmlOptions();
+        options.setCompileNoUpaRule();
+        options.setCompileNoValidation();
+        options.setCompileDownloadUrls();
+
+        /* Load the schema */
+        final XmlObject[] schemaRepresentation = new XmlObject[schemas.size()];
+        final SchemaTypeLoader contextTypeLoader = SchemaTypeLoaderImpl.build(new SchemaTypeLoader[] {BuiltinSchemaTypeSystem.get()}, null, XmlMetaDataFieldFactory.class.getClassLoader());
+        for (int i = 0; i < schemas.size(); i++)
+        {
+            schemaRepresentation[i] = contextTypeLoader.parse(schemas.get(i), null, null);
+        }
+        return SchemaTypeSystemCompiler.compile(null, null, schemaRepresentation, null, contextTypeLoader, null, options);
+    }
+}
