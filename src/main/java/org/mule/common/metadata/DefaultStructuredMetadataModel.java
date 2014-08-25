a60cf6b5cdf037c18a7167787c68c6210a967e83
Mariano Achaval <mariano.achaval@mulesoft.com>
2014-08-25 13:17:48 -0300

    Fixing Equals is not implemented

--- src/main/java/org/mule/common/metadata/DefaultStructuredMetadataModel.java
+++ src/main/java/org/mule/common/metadata/DefaultStructuredMetadataModel.java
@@ -5,29 +5,66 @@ import org.mule.common.metadata.datatype.DataType;
 import java.util.List;
 
 
-public class DefaultStructuredMetadataModel extends AbstractMetaDataModel implements StructuredMetaDataModel{
+public class DefaultStructuredMetadataModel extends AbstractMetaDataModel implements StructuredMetaDataModel
+{
 
     private List<MetaDataField> fields;
 
-    public DefaultStructuredMetadataModel(DataType dataType)  {
+    public DefaultStructuredMetadataModel(DataType dataType)
+    {
         super(dataType);
 
     }
 
-    public void init(MetaDataFieldFactory fieldFactory){
+    public void init(MetaDataFieldFactory fieldFactory)
+    {
         fields = fieldFactory.createFields();
     }
 
 
     @Override
-    public void accept(MetaDataModelVisitor modelVisitor) {
+    public void accept(MetaDataModelVisitor modelVisitor)
+    {
 
     }
 
-
+    @Override
+    public List<MetaDataField> getFields()
+    {
+        return fields;
+    }
 
     @Override
-    public List<MetaDataField> getFields() {
-        return fields;
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+        if (!super.equals(o))
+        {
+            return false;
+        }
+
+        DefaultStructuredMetadataModel that = (DefaultStructuredMetadataModel) o;
+
+        if (fields != null ? !fields.equals(that.fields) : that.fields != null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = super.hashCode();
+        result = 31 * result + (fields != null ? fields.hashCode() : 0);
+        return result;
     }
 }
