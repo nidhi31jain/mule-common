ceb8996634bba6699a1d035a9f42f243c87c21f3
fernandezlautaro <fernandez.lautaro@gmail.com>
2015-01-06 16:33:12 -0300

    Fix improvement/DEVKIT-1306: * Now MetaDataModel supports a new property that represents the key that's being described (cherry picked from commit 28a7c61)

---
+++ src/main/java/org/mule/common/metadata/property/StructureIdentifierMetaDataProperty.java
@@ -0,0 +1,32 @@
+package org.mule.common.metadata.property;
+
+
+import org.mule.common.metadata.MetaDataKey;
+import org.mule.common.metadata.MetaDataModelProperty;
+
+/**
+ * Property used to reference a key {@link #metaDataKey} from a concrete entity, describing also if the current entity
+ * is a derived one or not (see {@link #isDerivedStructure()})
+ */
+public class StructureIdentifierMetaDataProperty implements MetaDataModelProperty {
+    MetaDataKey metaDataKey;
+    boolean derivedStructure;
+
+    public StructureIdentifierMetaDataProperty(MetaDataKey metaDataKey, boolean derivedStructure) {
+        this.metaDataKey = metaDataKey;
+        this.derivedStructure = derivedStructure;
+    }
+
+    public MetaDataKey getMetaDataKey() {
+        return metaDataKey;
+    }
+
+    /**
+     * @return False if the {@link org.mule.common.metadata.MetaDataModel} that contains this property is describing an
+     * entity from the result of the list {@link org.mule.common.metadata.ConnectorMetaDataEnabled#getMetaDataKeys()}.
+     * True otherwise.
+     */
+    public boolean isDerivedStructure() {
+        return derivedStructure;
+    }
+}
