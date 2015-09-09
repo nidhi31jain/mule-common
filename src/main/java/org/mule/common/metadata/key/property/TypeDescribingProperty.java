c80f8f2f57eb942554a49cba69fa1f4b0295b53f
fernandezlautaro <fernandez.lautaro@gmail.com>
2015-09-09 18:09:03 -0300

    Because of serialization requirements on the Studio Catalog, this property needs to be used and cannot be provided by connectors

--- src/main/java/org/mule/common/metadata/key/property/TypeDescribingProperty.java
+++ src/main/java/org/mule/common/metadata/key/property/TypeDescribingProperty.java
@@ -3,9 +3,7 @@ package org.mule.common.metadata.key.property;
 /**
  * Property used between DevKit and the connector. This property will help the connector's developer to discriminate
  * between input or output metadata.
- * This object is deprecated, use {@link org.mule.common.metadata.property.CatalogTypeProperty} instead
  */
-@Deprecated
 public class TypeDescribingProperty implements MetaDataKeyProperty {
 
     private TypeScope typeScope;
