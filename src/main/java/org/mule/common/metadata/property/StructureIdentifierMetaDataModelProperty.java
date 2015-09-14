6f90223140e794afc0cb842ad80862875a8537de
fernandezlautaro <fernandez.lautaro@gmail.com>
2015-09-14 10:42:05 -0300

    DEVKIT-2006: new property for entities used between connectors and Studio when describing metadata (cherry picked from commit cb590df)

--- src/main/java/org/mule/common/metadata/property/StructureIdentifierMetaDataModelProperty.java
+++ src/main/java/org/mule/common/metadata/property/StructureIdentifierMetaDataModelProperty.java
@@ -1,20 +1,35 @@
 package org.mule.common.metadata.property;
 
 
+import org.mule.common.metadata.ConnectorMetaDataEnabled;
+import org.mule.common.metadata.MetaData;
 import org.mule.common.metadata.MetaDataKey;
+import org.mule.common.metadata.MetaDataModel;
 import org.mule.common.metadata.MetaDataModelProperty;
+import org.mule.common.metadata.OperationMetaDataEnabled;
 
 /**
  * Property used to reference a key {@link #metaDataKey} from a concrete entity, describing also if the current entity
- * is a derived one or not (see {@link #isDerivedStructure()})
+ * is a derived one or not (see {@link #isDerivedStructure()}) and if the current {@link #metaDataKey} should be
+ * displayed in Studio or not.
  */
 public class StructureIdentifierMetaDataModelProperty implements MetaDataModelProperty {
     MetaDataKey metaDataKey;
     boolean derivedStructure;
+    boolean generatedStructure;
 
+    /**
+     * Use {@link #StructureIdentifierMetaDataModelProperty(MetaDataKey, boolean, boolean)} instead
+     */
+    @Deprecated
     public StructureIdentifierMetaDataModelProperty(MetaDataKey metaDataKey, boolean derivedStructure) {
+        this(metaDataKey, derivedStructure, false);
+    }
+
+    public StructureIdentifierMetaDataModelProperty(MetaDataKey metaDataKey, boolean derivedStructure, boolean generatedStructure) {
         this.metaDataKey = metaDataKey;
         this.derivedStructure = derivedStructure;
+        this.generatedStructure = generatedStructure;
     }
 
     public MetaDataKey getMetaDataKey() {
@@ -22,11 +37,21 @@ public class StructureIdentifierMetaDataModelProperty implements MetaDataModelPr
     }
 
     /**
-     * @return False if the {@link org.mule.common.metadata.MetaDataModel} that contains this property is describing an
-     * entity from the result of the list {@link org.mule.common.metadata.ConnectorMetaDataEnabled#getMetaDataKeys()}.
+     * @return False if the {@link MetaDataModel} that contains this property is describing an
+     * entity from the result of the list {@link ConnectorMetaDataEnabled#getMetaDataKeys()}.
      * True otherwise.
      */
     public boolean isDerivedStructure() {
         return derivedStructure;
+    }
+
+    /**
+     * @return False if the {@link #metaDataKey} that describes the {@link MetaDataModel} which contains this property
+     * was used for both operations: {@link OperationMetaDataEnabled#getOutputMetaData(MetaData)} and
+     * {@link OperationMetaDataEnabled#getInputMetaData()}.
+     * True otherwise.
+     */
+    public boolean isGeneratedStructure() {
+        return generatedStructure;
     }
 }
